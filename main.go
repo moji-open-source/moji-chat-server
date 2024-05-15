bb37528d8de23c418539ed25edb23bcef5574080
Clover <2621869236@qq.com>
2024-05-15 10:29:42 +0800

    chore: grpc server

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/moji-open-source/moji-chat-server/hello"
+	"google.golang.org/grpc"
+)
+
+type GrpcServer struct {
+	pb.UnimplementedSayHelloServer
+}
+
+func (*GrpcServer) Hello(ctx context.Context, req *pb.HelloReq) (*pb.HelloResp, error) {
+	log.Printf("received grpc req: %+v", req.String())
+	return &pb.HelloResp{Msg: fmt.Sprintf("hello world! %s", req.Name)}, nil
+}
 
 func main() {
-	log.Println("hello world")
+	listen, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		panic(err)
+	}
+
+	server := grpc.NewServer()
+
+	pb.RegisterSayHelloServer(server, &GrpcServer{})
+
+	log.Println("server run on 8081 port...")
+	err = server.Serve(listen)
+
+	if err != nil {
+		panic(err)
+	}
 }
