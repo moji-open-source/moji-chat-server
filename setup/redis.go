023a553a165ea31828d045fb24303b6e665adb58
Clover You <2621869236@qq.com>
2024-08-06 21:34:20 +0800

    fix: error config

--- setup/redis.go
+++ setup/redis.go
@@ -4,7 +4,7 @@ import "github.com/redis/go-redis/v9"
 
 func NewRedlisClient(env *Env) *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:       env.Address,
+		Addr:       env.RedisAddr,
 		Password:   env.RedisPassword, // 没有密码，默认值
 		DB:         env.RedisDatabase, // 默认DB 0
 		Username:   env.RedisUsername,
